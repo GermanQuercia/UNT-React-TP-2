7029cc0c943e49987378c090638c8ce85aaa2deb
German Leonel Espindola <german.espindola@gmail.com>
2023-11-30 14:25:29 -0300

    Update TaskItem.jsx

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,62 +1,70 @@
 import { useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
 import './styles.css'
 
-const TaskItem = ({ jsonObj, index, setListaDeTareas }) => {
+
+const TaskItem = ({ task, index, onUpdateCompleted, onDeleteTask }) => {
+
+    // const borrarTareaCorto = (index) => {
+    //     setListaDeTareas(prevLista => prevLista.filter((_, i) => i !== index)) //funca bien
+    // };
+
+    // const eliminarTarea = (id) => {
+    //     setListaDeTareas(prev => {
+    //         let listaFiltrada = prev.filter(t => t.id != id);
+    //         return listaFiltrada;
+    //     })
+    // }
+
+    // const borrarTarea = function (event) {
+    //     const idDelElemento = Number(event.target.id);
+
+    //     setListaDeTareas(function (prevLista) {
+    //         return prevLista.filter(function (_, i) {
+    //             return i !== idDelElemento;
+    //         });
+    //     });
+    // };
 
 
-    const borrarTareaCorto = (index) => {
-        setListaDeTareas(prevLista => prevLista.filter((_, i) => i !== index)) //funca bien
-    };
 
-    const eliminarTarea = (id) => {
-        setListaDeTareas(prev => {
-            let listaFiltrada = prev.filter(t => t.id != id);
-            return listaFiltrada;
-        })
+    function handleClickDeleteTask(id){
+        onDeleteTask(id);
     }
 
-    const borrarTarea = function (event) {
-        const idDelElemento = Number(event.target.id);
+    function handleClickCompleteTask(id) {      
 
-        setListaDeTareas(function (prevLista) {
-            return prevLista.filter(function (_, i) {
-                return i !== idDelElemento;
-            });
-        });
-    };
+        onUpdateCompleted(id);  //Llamamos a la función que recibimos como propiedad que está 
+                                //implementada en TaskList y que se encarga de actualizar la lista
 
-    
 
-    function completada(id) {
-        const idDelElemento = id;
 
-        setListaDeTareas((prevLista) => {
-            // Create a new array to avoid mutating the previous state
-            const updatedLista = [...prevLista];
-            // Update the completed status of the task at idDelElemento
-            updatedLista[idDelElemento] = { ...updatedLista[idDelElemento], completada: !updatedLista[idDelElemento].completada };
-            // Return the updated array
-            return updatedLista;
-        });
+        // const idDelElemento = id;
+
+        // setListaDeTareas((prevLista) => {
+        //     // Create a new array to avoid mutating the previous state
+        //     const updatedLista = [...prevLista];
+        //     // Update the completed status of the task at idDelElemento
+        //     updatedLista[idDelElemento] = { ...updatedLista[idDelElemento], completada: !updatedLista[idDelElemento].completada };
+        //     // Return the updated array
+        //     return updatedLista;
+        //});
     }
 
-    let clase = jsonObj.completada ? 'task-completa' : '';
+    let clase = task.isCompleted ? 'task-completa' : '';   //Para agregar estilos personalizados a las tareas completas
 
 
     return (
         <>
             <tr className={clase}>
-                <td>{index + 1}</td>
+                <td>{index + 1}</td>                
+                <td>{task.date}</td>
+                <td>{task.time}</td>
+                <td>{task.task}</td>
                 
-                <td>{jsonObj.date}</td>
-                <td>{jsonObj.time}</td>
-                <td>{jsonObj.task}</td>
                 
-                <td>
-                    {/* <Button variant="danger" id={`${index}`} onClick={borrarTarea}><i class="fas fa-trash"></i></Button> */}
-                    <button className='btn' onClick={() => completada(index)}><i className="fas fa-check text-success"></i></button>
-                    <button className='btn' onClick={() => eliminarTarea(jsonObj.id)}><i className="fas fa-trash text-danger"></i></button>
+                <td>                    
+                    <button className='btn' onClick={() => handleClickCompleteTask(task.id)}><i className="fas fa-check text-success"></i></button>
+                    <button className='btn' onClick={() => handleClickDeleteTask(task.id)}><i className="fas fa-trash text-danger"></i></button>
                 </td>
             </tr>
         </>
@@ -64,3 +72,4 @@ const TaskItem = ({ jsonObj, index, setListaDeTareas }) => {
 }
 
 export default TaskItem;
+
